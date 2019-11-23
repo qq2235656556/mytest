ea80cd9622f2394719badc1e311931bc52c4c891
huanghe <2235656556@qq.com>
2019-11-23 12:30:45 +0800

    生产环境

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -4,5 +4,6 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("开发环境");
         System.out.println("预览环境");
+        System.out.println("生产环境");
     }
 }
