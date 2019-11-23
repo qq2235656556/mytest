956282a612703432cc5edf5c32be570944949cf7
huanghe <2235656556@qq.com>
2019-11-23 12:35:36 +0800

    开发环境plus

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("开发环境");
-        System.out.println("预览环境");
-        System.out.println("生产环境");
+        System.out.println("开发环境plus");
     }
 }
